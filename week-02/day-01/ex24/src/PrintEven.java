e42b1587939ba791dd7f273770d3d900a2106fcc
Vajda Lilla <vajdalil.hu@gmail.com>
2017-11-28 14:40:42 +0100

    completed exercise 24

---
+++ week-02/day-01/ex24/src/PrintEven.java
@@ -0,0 +1,10 @@
+public class PrintEven {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 500; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
