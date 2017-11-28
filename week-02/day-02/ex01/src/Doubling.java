e44f8b31726948076534020bb62c10e073e3d7aa
Vajda Lilla <vajdalil.hu@gmail.com>
2017-11-28 16:24:34 +0100

    completed exercise 2

---
+++ week-02/day-02/ex01/src/Doubling.java
@@ -0,0 +1,12 @@
+public class Doubling {
+    public static void main(String[] args) {
+        int ak = doubling(123);
+        System.out.println(ak);
+
+    }
+
+    public static int doubling (int number) {
+        return number * 2;
+    }
+
+}
