4de5bb0c7a913ad3fb05f9a8382da2cfdf29f6c0
Vajda Lilla <vajdalil.hu@gmail.com>
2017-11-28 17:35:47 +0100

    created(ex.04): completed exercise

---
+++ week-02/day-02/ex04/src/Summing.java
@@ -0,0 +1,11 @@
+public class Summing {
+    public static void main(String[] args) {
+    int summing = sum(21, 310, 12012, 23);
+        System.out.println(summing);
+
+    }
+    public  static int sum(int number1, int number2, int number3, int number4){
+        return number1 + number2 + number3 + number4;
+    }
+
+}
