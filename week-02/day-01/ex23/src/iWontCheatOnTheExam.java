dc9ba995b36299f256c99fe8eddc5d082d76326c
Vajda Lilla <vajdalil.hu@gmail.com>
2017-11-28 14:23:39 +0100

    completed exercise 23

---
+++ week-02/day-01/ex23/src/iWontCheatOnTheExam.java
@@ -0,0 +1,8 @@
+public class iWontCheatOnTheExam {
+    public static void main(String[] args) {
+        int i = 100;
+        for (i = 0; i <100; i++) {
+            System.out.println("I won't cheat on the exam!");
+        }
+    }
+}
