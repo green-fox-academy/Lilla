a9b09bcb9a0408e15ea6b10e047afff38f852e57
Vajda Lilla <vajdalil.hu@gmail.com>
2017-11-28 17:08:29 +0100

    create(ex.03): completed

---
+++ week-02/day-02/ex03/src/Append.java
@@ -0,0 +1,10 @@
+public class Append {
+    public static void main(String[] args) {
+        String am = appendA("kuty");
+        System.out.println(am);
+    }
+
+    public static String appendA(String word) {
+        return word + "a";
+    }
+}
