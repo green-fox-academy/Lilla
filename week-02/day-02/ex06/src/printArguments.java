ad6df992103bbc6feec3ecc385c6392e9e5eca8e
Vajda Lilla <vajdalil.hu@gmail.com>
2017-11-28 20:27:01 +0100

    creat(ex06): started with inputs

---
+++ week-02/day-02/ex06/src/printArguments.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class printArguments {
+    public static void main(String[] args) {
+        System.out.println("Please write something: ");
+        Scanner input = new Scanner(System.in);
+        String parameter = input.next();
+
+
+    }
+
+}
