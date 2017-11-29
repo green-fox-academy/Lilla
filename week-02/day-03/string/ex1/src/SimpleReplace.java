1c40ea57e8d77a3fe991d4e0763425f2d67d7a3a
Vajda Lilla <vajdalil.hu@gmail.com>
2017-11-29 10:26:04 +0100

    finish(String ex1)

---
+++ week-02/day-03/string/ex1/src/SimpleReplace.java
@@ -0,0 +1,7 @@
+public class SimpleReplace {
+    public static void main(String[] args) {
+        String starWars = "In a dishwasher far far away";
+
+        System.out.println(starWars.replace ("dishwasher","galaxy" ));
+    }
+}
