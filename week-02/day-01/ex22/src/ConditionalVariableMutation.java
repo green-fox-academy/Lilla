913d0ec6da314ed613d6b303fb10291ad973caca
Vajda Lilla <vajdalil.hu@gmail.com>
2017-11-30 11:39:02 +0100

    finish(ex22)

--- week-02/day-01/ex22/src/ConditionalVariableMutation.java
+++ week-02/day-01/ex22/src/ConditionalVariableMutation.java
@@ -1,5 +1,43 @@
 public class ConditionalVariableMutation {
     public static void main(String[] args) {
+        int b = 13;
+        if (b < 10) {
+            String out2 = "More";
+            System.out.println(out2);
+        } else if (b > 20) {
+            String out2 = "Less";
+            System.out.println(out2);
+        } else {
+            String out2 = "Sweet";
+            System.out.println(out2);
+        }
+
+
+        int c = 123;
+        int credits = 50;
+        boolean isBonus = true;
+        if ( credits >= 50 && isBonus == false) {
+            System.out.println(c - 2);
+        } else if ( credits > 50) {
+            System.out.println(c - 1);
+        } else {
+            System.out.println(c);
+        }
+
+
+        int d = 8;
+        int time = 120;
+        if ( d % 4 == 0 && time <= 200) {
+            String out3 = "check";
+            System.out.println(out3);
+        } else if ( time > 200 ) {
+            String out3 = "Time out!";
+            System.out.println(out3);
+        } else {
+            String out3 = "Run Forest Run!";
+            System.out.println(out3);
+        }
 
     }
+
 }
