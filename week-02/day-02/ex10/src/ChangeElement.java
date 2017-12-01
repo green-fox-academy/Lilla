0c6d404460e779a1b0065eb96de6123d8bb4a136
Vajda Lilla <vajdalil.hu@gmail.com>
2017-12-01 11:22:01 +0100

    finish(ex10)

---
+++ week-02/day-02/ex10/src/ChangeElement.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ChangeElement {
+    public static void main(String[] args) {
+        int s[] = {1, 2, 3, 8, 5, 6};
+        s [3] = 4;
+        System.out.println(Arrays.toString(s));
+    }
+}
