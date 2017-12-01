6ed0a4f2e307e1427ed7098f29d9f01c018ef8bc
Vajda Lilla <vajdalil.hu@gmail.com>
2017-12-01 11:13:06 +0100

    finish(ex08)

---
+++ week-02/day-02/ex08/src/CompareLength.java
@@ -0,0 +1,13 @@
+public class CompareLength {
+    public static void main(String[] args) {
+        int[] p1 = {1, 2, 3};
+        int[] p2 = {4, 5};
+        if (p1.length < p2.length) {
+            System.out.println("p2 has more elements");
+        } else if (p1.length > p2.length) {
+            System.out.println("p1 has more elements");
+        } else {
+            System.out.println("p1 and p2 has same number of elements");
+        }
+    }
+}
