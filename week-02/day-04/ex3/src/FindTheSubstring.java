3363114408867ac08aa6d5db31669c50092c9e83
Vajda Lilla <vajdalil.hu@gmail.com>
2017-12-02 23:42:13 +0100

    finish(ex3)

--- week-02/day-04/ex3/src/FindTheSubstring.java
+++ week-02/day-04/ex3/src/FindTheSubstring.java
@@ -3,19 +3,15 @@ import java.util.Scanner;
 
 public class FindTheSubstring {
     public static void main(String[] args) {
-        System.out.println("please enter a sentence: ");
+        System.out.println("Please enter a sentence: ");
         Scanner scanner1 = new Scanner(System.in);
-        ArrayList <String> sentence = new ArrayList<> ();
-        while (scanner1.hasNext()) {
-            sentence.add(scanner1.next());
-            break;
-        }
+        String sentence = scanner1.nextLine();
+        System.out.println(sentence);
 
         System.out.println("Please enter a word from you sentence: ");
         Scanner scanner2 = new Scanner(System.in);
         String word = scanner2.next();
 
-        int index = sentence.indexOf(word);
-        System.out.println(index);
+        System.out.println(sentence.indexOf(word));
     }
 }
