df9d817ed184a442cf86bdff6efd25d411ef9b5e
Vajda Lilla <vajdalil.hu@gmail.com>
2017-12-04 17:27:54 +0100

    only on console appears the decrypted file

---
+++ week-03/day-01/ex07/src/DuplicatedChars.java
@@ -0,0 +1,32 @@
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DuplicatedChars {
+    public static void main(String[] args) {
+        Path path = Paths.get("dup.txt");
+        Path path2 = Paths.get("dcrypt.txt");
+        FileWriter writeMyFile = new FileWriter("dcrypt.txt");
+
+        List<String> decryptedList = new ArrayList<String>();
+        String decrypted = "";
+
+        try {
+            List <String> listOflines = Files.readAllLines(path);
+            for (int i = 0; i < listOflines.size(); i++) {
+                for (int j = 0; j < (listOflines.get(i).length()); j += 2) {
+                    //System.out.print(listOflines.get(i).charAt(j));
+                    decrypted = decrypted + listOflines.get(i).charAt(j);
+                }
+                //System.out.println();
+                decryptedList.add(decrypted);
+            }
+            FileWriter(path2, decryptedList);
+        } catch (Exception e){
+                System.out.println("Error");
+        }
+    }
+}
