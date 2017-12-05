4b8660034f0df83c50303d3fb1531807e59c418f
Vajda Lilla <vajdalil.hu@gmail.com>
2017-12-05 22:51:19 +0100

    create(ex08) the text appears in reverse order, but inline

--- week-03/day-01/ex08/src/ReversedLines.java
+++ week-03/day-01/ex08/src/ReversedLines.java
@@ -8,22 +8,21 @@ public class ReversedLines {
     public static void main(String[] args) {
         Path path = Paths.get("rev.txt");
         Path path2 = Paths.get("new.txt");
-        //FileWriter writeMyFile = new FileWriter("new.txt");
 
-        List<String> originalList = new ArrayList<>();
-        String revList = "";
+        List<String> decryptedList = new ArrayList<>();
+        String decrypted = "";
 
         try {
             List <String> listOflines = Files.readAllLines(path);
             for (int i = 0; i < listOflines.size(); i++) {
                 for (int j = 0; j < (listOflines.get(i).length()); j ++) {
                     System.out.print(listOflines.get(i).charAt(listOflines.get(i).length() - j -1));
-                    //decrypted = decrypted + listOflines.get(i).charAt(j);
+                    decrypted = decrypted + listOflines.get(i).charAt(listOflines.get(i).length() - j - 1);
                 }
                 System.out.println();
-                //decryptedList.add(decrypted);
             }
-            //FileWriter(path2, decryptedList);
+            decryptedList.add(decrypted);
+            Files.write(path2, decryptedList);
         } catch (Exception e){
             System.out.println("Error");
         }
