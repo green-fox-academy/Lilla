87fcf284e8ad563ed93c89dae582e4a9fb157abd
Vajda Lilla <vajdalil.hu@gmail.com>
2017-12-11 14:07:51 +0100

    fix(ex02): made fields private

--- week-04/day-01/ex02/src/BlogPost.java
+++ week-04/day-01/ex02/src/BlogPost.java
@@ -1,6 +1,39 @@
 public class BlogPost {
-    String authorName;
-    String title;
-    String text;
-    String publicationDate;
+    private String authorName;
+    private String title;
+    private String text;
+    private String publicationDate;
+
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(String publicationDate) {
+        this.publicationDate = publicationDate;
+    }
 }
--- week-04/day-01/ex02/src/Main.java
+++ week-04/day-01/ex02/src/Main.java
@@ -1,29 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         BlogPost post1 = new BlogPost();
-        post1.authorName = "John Doe";
-        post1.title = "Lorem Ipsum";
-        post1.text = "Lorem ipsum dolor sit amet.";
-        post1.publicationDate = "2000.05.04.";
+        post1.setAuthorName( "John Doe" );
+        post1.setTitle("Lorem Ipsum");
+        post1.setText("Lorem ipsum dolor sit amet.");
+        post1.setPublicationDate("2000.05.04.");
 
-        System.out.println(post1.title + " titled by  " + post1.authorName + " at " + post1.publicationDate +  "\n" + post1.text + "\n");
 
         BlogPost post2 = new BlogPost();
-        post2.authorName = "Tim Urban";
-        post2.title = "Wait but why";
-        post2.text = "A popular long-form, stick-figure-illustrated blog about almost everything.";
-        post2.publicationDate = "2010.10.10.";
-
-        System.out.println(post2.title + " titled by  " + post2.authorName + " at " + post2.publicationDate +  "\n" + post2.text + "\n");
+        post2.setAuthorName("Tim Urban");
+        post2.setTitle("Wait but why");
+        post2.setText("A popular long-form, stick-figure-illustrated blog about almost everything.");
+        post2.setPublicationDate("2010.10.10.");
 
 
         BlogPost post3 = new BlogPost();
-        post3.authorName = "William Turton";
-        post3.title = "One Engineer Is Trying to Get IBM to Reckon With Trump";
-        post3.text = "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.";
-        post3.publicationDate = "2017.03.28.";
-
-        System.out.println(post3.title + " titled by  " + post3.authorName + " at " + post3.publicationDate +  "\n" + post3.text + "\n");
+        post3.setAuthorName("William Turton");
+        post3.setTitle("One Engineer Is Trying to Get IBM to Reckon With Trump");
+        post3.setText("Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.");
+        post3.setPublicationDate("2017.03.28.");
 
 
     }
