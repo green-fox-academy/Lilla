e86b2f2d9193e01a6912c2231f9eb25ff4c9c51e
Vajda Lilla <vajdalil.hu@gmail.com>
2017-12-11 13:54:37 +0100

    fix(ex02): printed in one line

--- week-04/day-01/ex02/src/Main.java
+++ week-04/day-01/ex02/src/Main.java
@@ -6,9 +6,7 @@ public class Main {
         post1.text = "Lorem ipsum dolor sit amet.";
         post1.publicationDate = "2000.05.04.";
 
-        System.out.println(post1.title + " titled by  " + post1.authorName + " at " + post1.publicationDate);
-        System.out.println(post1.text);
-        System.out.println();
+        System.out.println(post1.title + " titled by  " + post1.authorName + " at " + post1.publicationDate +  "\n" + post1.text + "\n");
 
         BlogPost post2 = new BlogPost();
         post2.authorName = "Tim Urban";
@@ -16,9 +14,8 @@ public class Main {
         post2.text = "A popular long-form, stick-figure-illustrated blog about almost everything.";
         post2.publicationDate = "2010.10.10.";
 
-        System.out.println(post2.title + " titled by  " + post2.authorName + " at " + post2.publicationDate);
-        System.out.println(post2.text);
-        System.out.println();
+        System.out.println(post2.title + " titled by  " + post2.authorName + " at " + post2.publicationDate +  "\n" + post2.text + "\n");
+
 
         BlogPost post3 = new BlogPost();
         post3.authorName = "William Turton";
@@ -26,9 +23,8 @@ public class Main {
         post3.text = "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.";
         post3.publicationDate = "2017.03.28.";
 
-        System.out.println(post3.title + " titled by  " + post3.authorName + " at " + post3.publicationDate);
-        System.out.println(post3.text);
-        System.out.println();
+        System.out.println(post3.title + " titled by  " + post3.authorName + " at " + post3.publicationDate +  "\n" + post3.text + "\n");
+
 
     }
 }
