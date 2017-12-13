e5905a5e89c38df0bfc63a8c0db884205cc40e76
Vajda Lilla <vajdalil.hu@gmail.com>
2017-12-13 15:04:28 +0100

    fix(ex4): now the letters shows how many times do they appear in a string

--- week-04/day-03/ex4/Tests/CountLettersTest.java
+++ week-04/day-03/ex4/Tests/CountLettersTest.java
@@ -6,8 +6,8 @@ public class CountLettersTest {
     @Test
     public void makeDictionary() throws Exception {
         CountLetters example = new CountLetters();
-        String word = "gyopár";
-        assertEquals(word.length() - 1, example.makeDictionary(word).get('r') );
+        String word = "gyalogkakukk";
+        assertEquals(4, example.makeDictionary(word).get('k') );
     }
 
 }
--- week-04/day-03/ex4/src/CountLetters.java
+++ week-04/day-03/ex4/src/CountLetters.java
@@ -6,10 +6,30 @@ public class CountLetters {
     public Map makeDictionary(String word){
         Map<Character, Integer> dictionary = new HashMap<>();
         for (int i = 0; i < word.length(); i++) {
+            char x = word.charAt(i);
+            Integer freqency = dictionary.get(x);
             dictionary.put(word.charAt(i), i);
+            if (freqency != null) {
+                dictionary.put(x, new Integer (freqency + 1));
+            } else {
+                dictionary.put(x,1);
+            }
         }
         System.out.println(dictionary);
         return dictionary;
     }
+
+
+    /**HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+    String s = "aasjjikkk";
+        for(int i = 0; i < s.length(); i++){
+        char c = s.charAt(i);
+        Integer val = map.get(c);
+        if(val != null){
+            map.put(c, new Integer(val + 1));
+        }else{
+            map.put(c,1);
+        }
+    }**/
 
 }
