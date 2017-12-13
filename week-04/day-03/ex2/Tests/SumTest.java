9f599ec80b0fd850565fdc904169484faf42593d
Vajda Lilla <vajdalil.hu@gmail.com>
2017-12-13 11:55:27 +0100

    feat(ex2): testedprogram for many cases

--- week-04/day-03/ex2/Tests/SumTest.java
+++ week-04/day-03/ex2/Tests/SumTest.java
@@ -7,15 +7,38 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class SumTest {
-    @Test
+    /** @Test
     public void sum() throws Exception {
         Sum testList = new Sum();
         Integer [] array = {1,2,3,4};
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(array));
 
         assertEquals(10, testList.sum(list));
-
-
     }
 
+    @Test
+    public void sum() throws Exception {
+        Sum testList = new Sum();
+        Integer [] array = {1};
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(array));
+
+        assertEquals(1, testList.sum(list));
+    }
+
+    @Test
+    public void sum() throws Exception {
+        Sum testList = new Sum();
+        Integer [] array = {};
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(array));
+
+        assertEquals(0, testList.sum(list));
+    }**/
+
+    @Test
+    public void sum() throws Exception {
+        Sum testList = new Sum();
+        Integer [] array = null;
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(array));
+        assertEquals(null, testList.sum(list));
+    }
 }
