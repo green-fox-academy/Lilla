cf51e3bd18e90b70876940bb6eaa00cee7fa5ffb
Vajda Lilla <vajdalil.hu@gmail.com>
2018-01-08 17:00:06 +0100

    feat(web APP) can get color randomly for hellos from a color array

--- week-07/day-01/practice/src/main/java/com/greenfoxacademy/springintro/controllers/HelloWebController.java
+++ week-07/day-01/practice/src/main/java/com/greenfoxacademy/springintro/controllers/HelloWebController.java
@@ -14,17 +14,37 @@ public class HelloWebController {
           "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
           "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
           "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+  String[] color = {"AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond",
+          "Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue",
+          "Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki",
+          "DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue",
+          "DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue",
+          "FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green",
+          "GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon",
+          "LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon",
+          "LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen",
+          "Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue",
+          "MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite",
+          "Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise",
+          "PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue",
+          "SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey",
+          "Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"};
+
 
   private AtomicLong countClick = new AtomicLong(0);
   Random random = new Random();
 
   @RequestMapping("/web/greeting")
   public String greeting(Model model, @RequestParam(value = "name", required = false) String name) {
-    int randomNumber = random.nextInt(hellos.length - 1);
+    int randomHello = random.nextInt(hellos.length - 1);
+    int randomColor = random.nextInt(color.length - 1);
+    int randomSize = random.nextInt(50);
     countClick.getAndIncrement();
     model.addAttribute("name", name);
     model.addAttribute("count", countClick);
-    model.addAttribute("hello", hellos[randomNumber]);
+    model.addAttribute("hello", hellos[randomHello]);
+    model.addAttribute("color", color[randomColor]);
+    model.addAttribute("size", randomSize);
     return "greeting";
   }
 }
