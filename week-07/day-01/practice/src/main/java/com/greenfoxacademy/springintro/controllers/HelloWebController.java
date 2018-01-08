04f59cf2cff318b1231ed6c140358f491af1d364
Vajda Lilla <vajdalil.hu@gmail.com>
2018-01-08 16:23:13 +0100

    feat(web APP) can get greeting randmly from hellos array

--- week-07/day-01/practice/src/main/java/com/greenfoxacademy/springintro/controllers/HelloWebController.java
+++ week-07/day-01/practice/src/main/java/com/greenfoxacademy/springintro/controllers/HelloWebController.java
@@ -5,17 +5,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
+  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+          "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+          "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+          "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
 
   private AtomicLong countClick = new AtomicLong(0);
+  Random random = new Random();
+
   @RequestMapping("/web/greeting")
   public String greeting(Model model, @RequestParam(value = "name", required = false) String name) {
+    int randomNumber = random.nextInt(hellos.length - 1);
     countClick.getAndIncrement();
     model.addAttribute("name", name);
     model.addAttribute("count", countClick);
+    model.addAttribute("hello", hellos[randomNumber]);
     return "greeting";
   }
 }
