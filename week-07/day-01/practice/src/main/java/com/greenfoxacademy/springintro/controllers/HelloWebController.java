d0db8b3105e5a1628edb0add5c4fe75167a1829c
Vajda Lilla <vajdalil.hu@gmail.com>
2018-01-08 16:13:11 +0100

    feat(web APP) can count api calls

--- week-07/day-01/practice/src/main/java/com/greenfoxacademy/springintro/controllers/HelloWebController.java
+++ week-07/day-01/practice/src/main/java/com/greenfoxacademy/springintro/controllers/HelloWebController.java
@@ -3,13 +3,19 @@ package com.greenfoxacademy.springintro.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
 
+  private AtomicLong countClick = new AtomicLong(0);
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(Model model, @RequestParam(value = "name", required = false) String name) {
+    countClick.getAndIncrement();
+    model.addAttribute("name", name);
+    model.addAttribute("count", countClick);
     return "greeting";
   }
 }
