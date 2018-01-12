c2545dd0a0097b419680c2af198b0e79ce3894e8
Vajda Lilla <vajdalil.hu@gmail.com>
2018-01-12 13:19:15 +0100

    feat(index.hrml and MainController) created html and a maincontroller, that calls through /main route

---
+++ week-07/day-05/fox/src/main/java/com/greenfoxacademy/foxapp/FoxappApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.foxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxappApplication.class, args);
+	}
+}
