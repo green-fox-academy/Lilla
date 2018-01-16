214ff96d7ad7739fcf48768612fea621de46b466
Vajda Lilla <vajdalil.hu@gmail.com>
2018-01-16 14:58:41 +0100

    edit function is working, my todo app is working well, needs only some beauty things

--- week-08/connection/src/main/java/com/greenfoxacademy/connection/controllers/TodoController.java
+++ week-08/connection/src/main/java/com/greenfoxacademy/connection/controllers/TodoController.java
@@ -5,10 +5,7 @@ import com.greenfoxacademy.connection.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 import javax.servlet.http.HttpServletRequest;
@@ -31,8 +28,7 @@ public class TodoController {
   @RequestMapping(value = "/list", params = {"delete"})
   public String delete(final HttpServletRequest request) {
     final Integer index = Integer.valueOf(request.getParameter("delete"));
-    Todo todo = todoRepository.findOne(index);
-    todoRepository.delete(todo.getId());
+    todoRepository.delete(index);
     return "redirect:list";
   }
 
@@ -41,7 +37,7 @@ public class TodoController {
     List<Todo> active = new ArrayList<>();
     List<Todo> todos = (List<Todo>) todoRepository.findAll();
     for (Todo todo : todos) {
-      if (todo.isDone() == false) {
+      if (todo.getIsDone() == false) {
         active.add(todo);
       }
     }
@@ -61,18 +57,17 @@ public class TodoController {
     return "redirect:/list";
   }
 
-  //todo edithet kapcsolódik
-
-  @GetMapping("{id}/edit")
-  public String edit(Model model, @PathVariable int id) {
-    Todo todo = todoRepository.findOne(id);
+  @GetMapping("/edit/{id}")
+  public String edit(Model model, @ModelAttribute Todo todo, @PathVariable int id) {
+    todo = todoRepository.findOne(id);
     model.addAttribute("todo", todo);
     return "edit";
   }
 
-  @PostMapping("/edit/todo")
-  public String createUpdate(Todo todo) {
+  @PostMapping("/edit/{id}")
+  public String editTodo( @ModelAttribute Todo todo, @PathVariable int id) {
+    todo.setId(id);
     todoRepository.save(todo);
-    return "redirect:/list";
+    return "redirect:list";
   }
 }
--- week-08/connection/src/main/java/com/greenfoxacademy/connection/models/Todo.java
+++ week-08/connection/src/main/java/com/greenfoxacademy/connection/models/Todo.java
@@ -50,19 +50,19 @@ public class Todo {
     this.title = title;
   }
 
-  public boolean isUrgent() {
+  public boolean getIsUrgent() {
     return isUrgent;
   }
 
-  public void setUrgent(boolean urgent) {
+  public void setIsUrgent(boolean urgent) {
     isUrgent = urgent;
   }
 
-  public boolean isDone() {
+  public boolean getIsDone() {
     return isDone;
   }
 
-  public void setDone(boolean done) {
+  public void setIsDone(boolean done) {
     isDone = done;
   }
 }
